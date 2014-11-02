7790e73f536b5ab57110e1e4efad54919d570474
radekcerman <info@radekcerman.cz>
2014-11-02 19:40:28 +0100

    nový header

--- js/resolution-test.js
+++ js/resolution-test.js
@@ -1,20 +1,11 @@
 function adjustStyle(width) {
     width = parseInt(width);
     if ((width < 400)) {
-        $("#size-stylesheet").attr("href", "../css/narrow.css");
-
-        $("#img1").attr("src", "../img/slide/wide/1.jpg");
-        
+        $("#size-stylesheet").attr("href", "../css/narrow.css");        
     } else if ((width >= 401) && (width < 800)) {
-        $("#size-stylesheet").attr("href", "../css/middle.css");
-
-        $("#img1").attr("src", "../img/slide/wide/2.jpg");
-        
+        $("#size-stylesheet").attr("href", "../css/middle.css");        
     } else {
-        $("#size-stylesheet").attr("href", "../css/wide.css");
-
-        $("#img1").attr("src", "../img/slide/wide/3.jpg");
-        
+        $("#size-stylesheet").attr("href", "../css/wide.css");        
     }
     
 }
