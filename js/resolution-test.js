2d1d035fdd7dc5a8d452fd26eda617d2aea66b4a
radekcerman <info@radekcerman.cz>
2014-11-05 23:30:25 +0100

    js fix a gurukul page

--- js/resolution-test.js
+++ js/resolution-test.js
@@ -1,16 +1,28 @@
 function adjustStyle(width) {
     width = parseInt(width);
-    if ((width < 400)) {
-        $("#size-stylesheet").attr("href", "../css/narrow.css");        
-    } else if ((width >= 401) && (width < 800)) {
-        $("#size-stylesheet").attr("href", "../css/middle.css");        
-    } else {
-        $("#size-stylesheet").attr("href", "../css/wide.css");        
-    }
+    var deep = $("#deep").attr("class");
     
+    var sdeep = "";
+    if (deep == 1)
+        sdeep = "../";
+    else if (deep == 2)
+        sdeep = "../../";
+    else if (deep == 3)
+        sdeep = "../../../";
+    else if (deep == 4)
+        sdeep = "../../../../";
+    
+    if ((width < 400)) {
+        $("#size-stylesheet").attr("href", sdeep + "css/narrow.css");        
+    } else if ((width >= 401) && (width < 800)) {
+        $("#size-stylesheet").attr("href", sdeep + "css/middle.css");
+    } else {
+        $("#size-stylesheet").attr("href", sdeep + "css/wide.css");
+    }
+    //console.log($("#size-stylesheet").attr("href"));
 }
 
-$(function () {
+$(function adjust() {
     
     adjustStyle($(this).width());
     $(window).resize(function() {
